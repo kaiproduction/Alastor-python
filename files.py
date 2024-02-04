bb9573c82bc91ffd72f770fd826c5b387e2c04e3
kai studio <97531705+kaiproduction@users.noreply.github.com>
2024-02-05 00:15:36 +0200

    Create files.py

---
+++ files.py
@@ -0,0 +1,10 @@
+# alastor/files.py
+
+class FileManager:
+    def read(self, file_path):
+        # Реализация метода для чтения данных из файла
+        pass
+
+    def write(self, file_path, data):
+        # Реализация метода для записи данных в файл
+        pass

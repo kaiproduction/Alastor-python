22b82e57129a3ecf495b5a7a1ba699f9c6a09658
kai studio <97531705+kaiproduction@users.noreply.github.com>
2024-02-05 00:14:43 +0200

    Create db.py

---
+++ db.py
@@ -0,0 +1,10 @@
+# alastor/db.py
+
+class DatabaseManager:
+    def query(self, query):
+        # Реализация метода для выполнения запроса к базе данных
+        pass
+
+    def save(self, data):
+        # Реализация метода для сохранения данных в базу данных
+        pass
